d1b6467699654b3903996341cabe4b72bc264a3b
Alex Vanston <alex@mrvdot.com>
2013-08-06 15:52:54 -0600

    Initial component commit

---
+++ components/angular-mvd-tunnels/README.md
@@ -0,0 +1,86 @@
+# Angular Tunnels
+
+Angular tunnels provides a way of creating a communication tunnel between two or more directives/controllers without requiring them to be part of the same DOM tree (like is needed for `$emit` and `$broadcast`);
+
+## Installation
+To install tunnels, you can either clone this repository directly, or use bower to install:
+
+```sh
+bower install mrvdot/angular-tunnels
+```
+
+Then include the `tunnels.js` file into your html and require `mvd.tunnels` in your module:
+
+```javascript
+angular.module('myApp', ['mvd.tunnels']);
+```
+
+## Usage
+Tunnels works by registering events within a map that can be injected directly into controllers and directives. You can also use a directive controller for slightly more convenient usage.
+
+**Directive usage** (Preferred method)
+
+```html
+<div id="dir1" my-awesome-directive mvd-tunnel="awesomeness"></div>
+<div id="dir2" my-awesome-directive mvd-tunnel="awesomeness"></div>
+```
+
+```javascript
+angular.module('myApp')
+    .directive('myAwesomeDirective', function () {
+        var count = 1;
+        return {
+            require : 'mvdTunnel',//Grab the tunnel controller
+            link : function ($scope, $element, $attrs, tunnel) {
+                count++;//Count how many instances of this directive we have initialized
+                //Within directive, we're automatically namespaced to the tunnel attribute value,
+                //or '*' global namespace if none is specified
+                tunnel.listen('myMessage', function (message, params) {
+                    console.log(message);
+                    // output: { "tunnel" : "*", "message" : "myMessage" }
+                    console.log(params);
+                    // output: { "hello" : "tunnels", "count" : 2 }
+                });
+                
+                if (count > 1) {
+                    tunnel.send('myMessage', {
+                        "hello" : "tunnels",
+                        "count" : count
+                    });
+                }
+            }
+        }
+    })
+```
+
+**Direct Map Usage**
+
+```javascript
+angular.module('myApp')
+    .controller('MyCtrl', function (mvdTunnelMap) {    
+        //'*' is the default global namespace, if using the map directly you must specify this
+        mvdTunnelMap.listen('*', 'myMessage', function (message, myThought) {
+            console.log(message);
+            // output: { "tunnel" : "*", "message" : "myMessage" }
+            console.log(myThought);
+            // output: "Hello tunnels" (first argument passed to send below)
+        });
+    })
+    .controllter('OtherCtrl', function (mvdTunnelMap) {
+        mvdTunnelMap.send('*', 'myMessage', 'Hello tunnels');
+    });
+```
+
+**Additional information**
+###Digest Lifecycle###
+Much like `$emit` and `$broadcast`, all messages sent through the tunnel will be evaluated as part of the Angular scope `$digest` lifecycle, so any data-model changes made as part of your listeners will be reflected immediately.
+
+###Unsubscribing###
+Version 0.0.5 brought the ability to unsubscribe, following the same pattern as the built-in `$on` method. This means the return value of `listen` is a function that when executed will remove your callback from the subscription list. See below for an example:
+
+```javascript
+var off = mvdTunnelMap.listen('example','message', function () {
+  alert("I'm only fired once");
+  off();//Unsubscribe
+});
+```
---
+++ scripts/comments.js
@@ -0,0 +1,403 @@
+(function (angular) {
+  angular.module('mvd.comments', ['mvd.tunnels'])
+  .factory('commentConfig', function ($location, mvdTunnelMap) {
+    var siteId
+      , provider
+      , displayOnLoad = true
+      , baseUrl
+      , userCallbacks = {}
+      , callbacks = {
+        onCommentPosted : angular.noop,
+        onCommentCountUpdated : function (slug, count) {
+          mvdTunnelMap.send('comments','comment-count-updated', slug, count);
+          mvdTunnelMap.send('comments','comment-count-updated-' + slug, slug, count);
+        },
+        onThreadLoaded : function (slug, container) {
+          mvdTunnelMap.send('comments','thread-loaded', slug);
+          mvdTunnelMap.send('comments','thread-loaded-' + slug);
+        }
+      }
+      , methods = {
+      setSiteId : function (id) {
+        siteId = id;
+        return methods;
+      },
+      getSiteId : function () {
+        return siteId;
+      },
+      //Alias for Disqus
+      setForumName : function (name) {
+        return methods.setSiteId(name);
+      },
+      setProvider : function (prov) {
+        provider = prov;
+        return methods;
+      },
+      getProvider : function () {
+        return provider;
+      },
+      getDisplayOnLoad : function () {
+        return displayOnLoad;
+      },
+      getBaseUrl : function () {
+        return baseUrl || $location.get('protocol') + '://' + $location.get('host') + '/';
+      },
+      setBaseUrl : function (url) {
+        if (url.charAt(url.length - 1) != '/') {
+          url += '/';
+        };
+        baseUrl = url;
+        return methods;
+      },
+      setCallback : function (callback, fn) {
+        if (angular.isObject(callback)) {
+          return setCallbacks(callback);
+        } else {
+          userCallbacks[callback] = fn;
+          return methods;
+        }
+      },
+      setCallbacks : function (cbs) {
+        for (cb in cbs) {
+          if (!cbs.hasOwnProperty(cb)) {
+            continue;
+          };
+          methods.setCallback(cb, cbs[cb]);
+        }
+        return methods;
+      },
+      getCallbacks : function() {
+        return callbacks;
+      },
+      getCallback : function (callback) {
+        var intCB = callbacks[callback] || angular.noop;
+        var fullCB =  !userCallbacks[callback] ? intCB : 
+          function () {
+            intCB.apply(this, arguments);
+            userCallbacks[callback].apply(this, arguments);
+          }
+        return fullCB;
+      }
+    };
+
+    return methods;
+  })
+  .factory('livefyre', function (commentConfig, mvdTunnelMap) {
+    var widget
+      , container
+      , lastSlug
+      , loaded = false
+      , siteId = commentConfig.getSiteId()
+      , baseUrl = commentConfig.getBaseUrl()
+      , displayOnLoad = commentConfig.getDisplayOnLoad();
+
+    (function () {
+      var s = document.createElement('script'); s.async = true;
+      s.type = 'text/javascript';
+      s.src = 'http://zor.livefyre.com/wjs/v3.0/javascripts/livefyre.js';
+      (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+      var c = document.createElement('script'); c.async = true;
+      c.type = 'text/javascript';
+      c.src = 'http://zor.livefyre.com/wjs/v1.0/javascripts/CommentCount.js';
+      (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(c);
+    })();
+
+    var loadCount = function (slug, $element, title, url) {
+      if (!siteId) {
+        throw Error("Must specify siteId before initializing lifefyre");
+      };
+      if (!window.LF) {
+        setTimeout(function () {
+          loadCount(slug, $element, title, url);
+        }, 100);
+        return;
+      };
+      LF.CommentCount.fetch([{
+        site_id : siteId,
+        article_id : slug
+      }], function (response) {
+        if (response[siteId][slug]) {
+          var count = response[siteId][slug].total;
+          commentConfig.getCallback('onCommentCountUpdated')(slug, count);
+        }
+      });
+
+      if (displayOnLoad && !lastSlug) {
+        loadThread(slug, $element, title, url);
+      };
+    }
+
+    var loadCollection = function (collection, $thread) {
+      if (!container) {
+        container = angular.element('<div id="livefyre-comments"></div>');
+      };
+      if (!widget) {
+        container.appendTo($thread);
+        fyre.conv.load({}, [collection], function(w) {
+          widget = w;
+          widget.on('commentPosted', function (data) {
+            commentConfig.getCallback('onCommentPosted')(lastSlug, data);
+          });
+
+          widget.on('commentCountUpdated', function (data) {
+            commentConfig.getCallback('onCommentCountUpdated')(lastSlug, data);
+          });
+
+          widget.on('initialRenderComplete', function () {
+            commentConfig.getCallback('onThreadLoaded')(lastSlug, container);
+          });
+        });
+      } else {
+        var off = mvdTunnelMap.listen('comments', 'thread-loaded-'+collection.articleId, function () {
+          container.appendTo($thread);
+          off();
+        });
+        widget.changeCollection(collection);
+      }
+    };
+
+    var loadThread = function (slug, $element, title, url) {
+      if (!siteId) {
+        throw Error("Must specify siteId before initializing lifefyre");
+      };
+      if (!window.fyre) {
+        setTimeout(function () {
+          loadThread(slug, $element, title, url);
+        }, 100);
+        return;
+      };
+      lastSlug = slug;
+      var $thread = $element.find('.thread');
+      if (!$thread.length) {
+        setTimeout(function () {
+          loadThread(slug, $element);
+        }, 100);
+        return;
+      }
+
+      var collection = {
+        el: 'livefyre-comments',
+        network: "livefyre.com",
+        siteId: siteId,
+        articleId: lastSlug,
+        signed: false,
+        collectionMeta: {
+          articleId: lastSlug,
+          url: fyre.conv.load.makeCollectionUrl(url || baseUrl + slug),
+          title : title
+        }
+      }
+
+      loadCollection(collection, $thread);
+    };
+
+    return {
+      loadCount : loadCount,
+      loadThread : loadThread
+    }
+  })
+  .factory('disqus', function (commentConfig, mvdTunnelMap, $http) {
+    var container
+      , lastSlug
+      , siteId = commentConfig.getSiteId()
+      , baseUrl = commentConfig.getBaseUrl()
+     , displayOnLoad = commentConfig.getDisplayOnLoad();
+    
+    window.disqus_shortname = siteId;
+
+    window.disqus_config = function () {
+      this.callbacks.onReady.push(function () {
+        commentConfig.getCallback('onThreadLoaded')(lastSlug, container);
+      });
+
+      this.callbacks.onNewComment.push(function (comment) {
+        commentConfig.getCallback('onCommentPosted')(lastSlug, comment);
+      });
+    }
+
+    //Load main disqus file
+    var loadDisqus = function() {
+      var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+      dsq.src = '//' + siteId + '.disqus.com/embed.js';
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    };
+
+    var loadThread = function (slug, $element, title, url) {
+      if (!siteId) {
+        throw Error("Must specify siteId before initializing disqus");
+      };
+      if (!container) {
+        container = angular.element('<div id="disqus_thread"></div>');
+      };
+      var $thread = $element.find('.thread');
+      if (!$thread.length) {
+        setTimeout(function () {
+          loadThread(slug, $element, title, url);
+        }, 100);
+        return;
+      }
+      if (lastSlug) {
+        var oldSlug = lastSlug;
+        setTimeout(function () {
+          loadCount(oldSlug);
+        })
+      };
+      lastSlug = slug;
+      if (!window.DISQUS) {
+        container.appendTo($thread);
+        window.disqus_identifier = slug;
+        if (url) {
+          window.disqus_url = url;
+        };
+        if (title) {
+          window.disqus_title = title;
+        };
+        loadDisqus();
+      } else {
+        var off = mvdTunnelMap.listen('comments', 'thread-loaded-'+slug, function () {
+          container.appendTo($thread);
+          off();
+        });
+        DISQUS.reset({
+          reload : true,
+          config : function () {
+            this.page.identifier = slug;
+            this.page.url = url;
+            this.page.title = title;
+          }
+        });
+      }
+    }
+
+    var loadCountFile = function () {
+      var s = document.createElement('script'); s.async = true;
+      s.type = 'text/javascript';
+      s.src = 'http://' + siteId + '.disqus.com/count.js';
+      (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+    };
+
+    var setupWidget = function () {
+      //Overloading regular callback
+      window.DISQUSWIDGETS = {
+        displayCount : function (data) {
+          var textMap = data.text;
+          var counts = data.counts;
+          if (!counts || !counts.length) {
+            return;
+          };
+          for (var i = 0, ii = counts.length; i < ii; i++) {
+            var c = counts[i];
+            var slug = loads[c.uid];
+            commentConfig.getCallback('onCommentCountUpdated')(slug, c.comments);
+          }
+        }
+      }
+    }
+
+    var lastLoad = -1;
+
+    var loads = {};
+
+    var loadCount = function (slug, $element) {
+      if (!siteId) {
+        throw Error("Must specify siteId before initializing disqus");
+      };
+      if (!window.DISQUSWIDGETS) {
+        setupWidget();
+      }
+      var uid = (++lastLoad);
+      var s = document.createElement('script'); s.async = true;
+      s.type = 'text/javascript';
+      s.src = 'http://'+siteId + '.disqus.com/count-data.js?q=1&' + uid + '=1,'+encodeURIComponent(slug);
+      (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+
+      loads[uid] = slug;
+
+      if (displayOnLoad && !lastSlug) {
+        //Auto load first comment thread
+        loadThread(slug, $element);
+      };
+    }
+
+    return {
+      loadCount : loadCount,
+      loadThread : loadThread
+    }
+  })
+  .factory('commentService', function (commentConfig, $injector) {
+    var commentProviderName = commentConfig.getProvider();
+    if (!commentProviderName) {
+      throw Error("Must specify provider name: (disqus/livefyre)");
+    };
+
+    return $injector.get(commentProviderName);
+  })
+  .directive('comments', function (commentService) {
+    var tpl = '<div class="comments">' + 
+      '<p class="comment-count" ng-hide="threadLoaded">' + 
+        '<span ng-click="loadThread()" ng-pluralize count="numComments" when="{\'-1\' : \'Loading comment count...\',  \'0\' : \'No comments yet, be the first!\', \'one\' : \'Somebody got FIRST! comment, care to join in?\', \'other\' : \'{} comments already, want to join in the conversation?\' }"></span>' + 
+        '<span class="loader" ng-show="loadingThread"><img src="themes/clean-blog/images/loader-small.gif" /></span>' + 
+      '</p>' + 
+      '<div class="thread" ng-show="threadLoaded">' + 
+      '</div>' + 
+    '</div>';
+
+    return {
+      template : tpl,
+      replace : true,
+      require : 'mvdTunnel',
+      scope : {
+        'element' : '=comments',
+        'elementId' : '=elementId'
+      },
+      link : function ($scope, $element, $attrs, tunnel) {
+        $scope.numComments = -1;
+        $scope.threadLoaded = false;
+        $scope.loadingThread = false;
+
+        var slug;
+
+        var attachListeners = function (slug) {
+          tunnel.on('comment-count-updated-'+slug, function (ev, targetSlug, count) {
+            if (angular.isUndefined(count)) {
+              return;
+            };
+            $scope.numComments = count;
+          });
+
+          tunnel.on('thread-loaded', function (ev, targetSlug) {
+            $scope.threadLoaded = (targetSlug == slug);
+            $scope.loadingThread = false;
+          });
+        }
+
+        $scope.$watch('element.slug', function (nv, ov) {
+          if (!nv) {
+            return;
+          };
+          slug = nv;
+          var title = $scope.element.title || $scope.element.name;
+          var url = $scope.element.url || undefined;
+          attachListeners(slug);
+          commentService.loadCount(nv, $element, title, url);
+        });
+
+        $scope.$watch('elementId', function (nv, ov) {
+          if (!nv) {
+            return;
+          };
+          slug = nv;
+          attachListeners(slug);
+          commentService.loadCount(nv, $element);
+        });
+
+        $scope.loadThread = function () {
+          $scope.loadingThread = true;
+          var title = $scope.element && $scope.element.title || undefined;
+          var url = $scope.element && $scope.element.url || undefined;
+          commentService.loadThread(slug, $element, title, url);
+        };
+      }
+    }
+  });
+})(angular);
